70814556a3c74fab967bfa1a0dfd61e7d81a2c7b
Md Forhad <forhadhossain1595@gmail.com>
2026-09-17 01:01:49 +0600

    Initial portfolio page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-5xl font-bold text-black">
+        Md Forhad
+      </h1>
+    </main>
+  );
+}
